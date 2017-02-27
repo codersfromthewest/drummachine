148a465ae12931e504eeb858ba832dd56af62172
Darius Marte <Darius.marte66gmail.com>
2017-02-27 18:08:43 -0500

    still drummin

--- app.js
+++ app.js
@@ -24,20 +24,20 @@
 //       audio.play();
 //     }
 //   }
-// } 
+// }  
 
-  function removeTransition(event) {
-    if (e.propertyName !== 'transform') return;
-    e.target.classList.remove('playing');
-  }
-  function playSound(event) {
-    const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
-    const key = document.querySelector(`div[data-key="${event.keyCode}"]`);
-    if (!audio) return;
-    key.classList.add('playing');
-    audio.currentTime = 10;
-    audio.play();
-  }
-  const keys = Array.from(document.querySelectorAll('.key'));
-  keys.forEach(key => key.addEventListener('transitionend', removeTransition));
-  window.addEventListener('keydown', playSound);
+  // function removeTransition(event) {
+  //   if (e.propertyName !== 'transform') return;
+  //   e.target.classList.remove('playing');
+  // }
+  // function playSound(event) {
+  //   const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
+  //   const key = document.querySelector(`div[data-key="${event.keyCode}"]`);
+  //   if (!audio) return;
+  //   key.classList.add('playing');
+  //   audio.currentTime = 10;
+  //   audio.play();
+  // }
+  // const keys = Array.from(document.querySelectorAll('.key'));
+  // keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+  // window.addEventListener('keydown', playSound);
