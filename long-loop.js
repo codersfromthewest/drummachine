558977ff4a14c489beef43734a6d94c6967227d9
nicky evers <livelyevers@gmail.com>
2017-02-27 17:05:58 -0800

    added recorded sound loops, still have issues on logic

--- long-loop.js
+++ long-loop.js
@@ -1,33 +1,39 @@
 'use strict';
+var key_nums = [65, 83, 68, 70, 71, 72, 74, 75, 76];//key codes
+// var key_letters = ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ';']; //array of keys
+var key_sounds = ['sounds/bass1.mp3', 'sounds/birds.mp3', 'sounds/drumloop-1.mp3', 'sounds/dubstep-drums1.mp3', 'sounds/hip-hop-drums1.mp3', 'sounds/jazz.mp3', 'sounds/marley.mp3', 'sounds/taiko.mp3', 'sounds/tremelo.mp3'];
 
-var clickToRotate = document.getElementsByClassName('record-div');
-var clickToPlay = document.getElementsByClassName('audio-element');
 
-for(var i = 0; i < clickToRotate.length; i++) {
-  clickToRotate[i].addEventListener('click', clickHandlerRotate);
-}
+// var clickToRotate = document.getElementsByClassName('record-div');
+// var clickToPlay = document.getElementsByClassName('audio-element');
 
-for(var i = 0; i < clickToPlay.length; i++) {
-  clickToPlay[i].addEventListener('click', clickHandlerPlay);
-}
+window.addEventListener('keydown', keyHandler);
 
-function clickHandlerRotate(event) {
-  event.preventDefault();
-  if(event.target.getAttribute('class') === 'record-div'){
-    event.target.setAttribute('class', 'spinning-div');
-  } else {
-    event.target.setAttribute('class', 'record-div');
+function keyHandler (event) {
+  for (var i = 0; i < key_nums.length; i++) {
+    if (event.keyCode === key_nums[i]) {
+      // var filepath = key_sounds[i];
+      var audio = document.getElementById(event.keyCode.toString());
+      audio.src = key_sounds[i];
+      audio.play();
+      var rotate = document.getElementsByClassName('record-div');
+      if (rotate[i].getAttribute('class') === 'record-div'){
+        rotate[i].setAttribute('class', 'spinning-div');
+      } else {
+        rotate[i].setAttribute('class', 'record-div');
+      }
+    }
   }
 }
 
-function clickHandlerPlay(event) {
-  event.preventDefault();
-  if(event.target.getAttribute('ended')) {
-    event.target.play();
-  } else {
-    event.target.pause();
-  }
-}
+// for(var i = 0; i < clickToRotate.length; i++) {
+//   clickToRotate[i].addEventListener('keydown', clickHandlerRotate);
+// }
+
+// for(var j = 0; j < clickToPlay.length; j++) {
+//   clickToPlay[i].addEventListener('keydown', clickHandlerPlay);
+// }
+
 
 
 
