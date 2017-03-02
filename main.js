7253976dfd98271c9438c91bfce86a474ebc0a83
nicky evers <livelyevers@gmail.com>
2017-03-01 16:58:37 -0800

    got if else statement around main script to add local storage of last kit picked and user name

--- main.js
+++ main.js
@@ -4,40 +4,62 @@ var kitImage = document.getElementsByClassName('kitimage');
 var userForm = document.getElementById('name-form');
 var newUser;
 
-userForm.addEventListener('submit', submitHandler);
+if(localStorage.length === 0) {
+  userForm.addEventListener('submit', submitHandler);
 
-function User(name) {
-  this.name = name;
-  this.lastKit;
-}
+  function User(name) {
+    this.name = name;
+    this.lastKit;
+  }
 
-for(var i = 0; i < kitImage.length; i++) {
-  kitImage[i].addEventListener('click', clickHandler);
-}
+  for(var i = 0; i < kitImage.length; i++) {
+    kitImage[i].addEventListener('click', clickHandler);
+  }
 
-function submitHandler(event) {
-  event.preventDefault();
-  var nameNode = document.getElementById('text-input');
-  var userName = nameNode.value;
+  function submitHandler(event) {
+    event.preventDefault();
+    var nameNode = document.getElementById('text-input');
+    var userName = nameNode.value;
 
+    var formParent = document.getElementById('form');
+    var formChildren = document.getElementById('name-form', 'text-input', 'submit-button');
+    formParent.removeChild(formChildren);
+
+    newUser = new User(userName);
+    users.push(newUser);
+    console.log(newUser, users);
+
+    var headerMessage = document.getElementById('header-message');
+    headerMessage.textContent = 'Welcome, ' + userName;
+  }
+
+  function clickHandler(event) {
+    if(!newUser) {
+      alert('duuuddee, what\'s your name?');
+    } else {
+      var chosenKit = event.target.name;
+      console.log(chosenKit);
+      newUser.lastKit = chosenKit;
+      storage();
+    }
+  }
+} else {
+  var storedObjects = localStorage.getItem('storedObjects');
+  storedObjects = JSON.parse(storedObjects);
+  users = storedObjects;
+
+// to remove text input if already done
   var formParent = document.getElementById('form');
   var formChildren = document.getElementById('name-form', 'text-input', 'submit-button');
   formParent.removeChild(formChildren);
 
-  newUser = new User(userName);
-  users.push(newUser);
-  console.log(newUser, users);
-
+  // adds name to header 
   var headerMessage = document.getElementById('header-message');
-  headerMessage.textContent = 'Welcome, ' + userName;
-  // localStorage.setItem('name', username);
+  headerMessage.textContent = 'Welcome, ' + users[0].name;;
 }
 
-function clickHandler(event) {
-  if(!newUser) {
-    alert('duuuddee, what\'s your name?');
-  }
-  var chosenKit = event.target.name;
-  console.log(chosenKit);
-  newUser.lastKit = chosenKit;
+
+function storage() {
+  var storedObjects = JSON.stringify(users);
+  localStorage.setItem('storedObjects', storedObjects);
 }
